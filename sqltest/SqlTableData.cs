dc97453a1597583f994aec8b378d3a34342e8d21
User <yellowpeanut42@rambler.ru>
2022-09-29 14:23:36 +0300

    Optimized Fill function

--- sqltest/SqlTableData.cs
+++ sqltest/SqlTableData.cs
@@ -29,7 +29,7 @@ namespace sqltest
                 string queryClient = "SELECT Id FROM client ORDER BY RAND() LIMIT 1";
                 string querySeller = "SELECT Id FROM employees ORDER BY RAND() LIMIT 1";
                 string queryProduct = "SELECT Id FROM product ORDER BY RAND() LIMIT 1";
-                int Id = 0, ClientId = 0, SellerId = 0, ProductId = 0, Quantity = 0;
+                int ClientId = 0, SellerId = 0, ProductId = 0, Quantity = 0;
                 //DataTable dtClient = new DataTable(), dtSeller = new DataTable(), dtProduct = new DataTable();
                 conn.Open();
                 //MySqlDataAdapter msda = new MySqlDataAdapter(queryClient, connStr); msda.Fill(dtClient);
@@ -37,21 +37,20 @@ namespace sqltest
                 //msda = new MySqlDataAdapter(queryProduct, connStr); msda.Fill(dtProduct);
 
                 MySqlCommand cmClient, cmSeller, cmProduct, cmd;
-                MySqlDataReader drClient, drSeller, drProduct;
+                MySqlDataReader dr;
                 for (int i = 0; i < rowCount; i++)
                 {
                     cmClient = new MySqlCommand(queryClient, conn);
                     cmSeller = new MySqlCommand(querySeller, conn);
                     cmProduct = new MySqlCommand(queryProduct, conn);
-                    Id = i+1501;
-                    drClient = cmClient.ExecuteReader(); drClient.Read();
-                    ClientId = drClient.GetInt32(0); drClient.Close();
-                    drSeller = cmSeller.ExecuteReader(); drSeller.Read();
-                    SellerId = drSeller.GetInt32(0); drSeller.Close();
-                    drProduct = cmProduct.ExecuteReader(); drProduct.Read();
-                    ProductId = drProduct.GetInt32(0); drProduct.Close();
+                    dr = cmClient.ExecuteReader(); dr.Read();
+                    ClientId = dr.GetInt32(0); dr.Close();
+                    dr = cmSeller.ExecuteReader(); dr.Read();
+                    SellerId = dr.GetInt32(0); dr.Close();
+                    dr = cmProduct.ExecuteReader(); dr.Read();
+                    ProductId = dr.GetInt32(0); dr.Close();
                     Quantity = rnd.Next(1, 5);
-                    string query = $"INSERT INTO log VALUES({Id}, {ClientId}, {SellerId}, {ProductId}, {Quantity}, 20220{rnd.Next(1, 9)}{rnd.Next(1, 30).ToString("00")}{rnd.Next(0, 23).ToString("00")}{rnd.Next(0, 60).ToString("00")}{rnd.Next(0, 60).ToString("00")});";
+                    string query = $"INSERT INTO log VALUES({ClientId}, {SellerId}, {ProductId}, {Quantity}, 20220{rnd.Next(1, 9)}{rnd.Next(1, 30).ToString("00")}{rnd.Next(0, 23).ToString("00")}{rnd.Next(0, 60).ToString("00")}{rnd.Next(0, 60).ToString("00")});";
                     cmd = new MySqlCommand(query, conn);
                     cmd.ExecuteNonQuery();
                 }
